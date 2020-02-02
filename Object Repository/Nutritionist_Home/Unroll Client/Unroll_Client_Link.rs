50b5894709579169a7f1c9a1344ebf3139465dcf
Kunal Harkare <kunal.harkare@hof-university.de>
2020-02-02 21:57:30 +0100

    Added Unroll with Nutritionist Test Case

--- Object Repository/Nutritionist_Home/Unroll Client/Unroll_Client_Link.rs
+++ Object Repository/Nutritionist_Home/Unroll Client/Unroll_Client_Link.rs
@@ -1,16 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>Add_Prescribtion</name>
+   <name>Unroll_Client_Link</name>
    <tag></tag>
-   <elementGuidId>056b6f61-4411-4f9c-9dd7-37e95f1ed7c2</elementGuidId>
+   <elementGuidId>b3152176-f51e-47e4-8172-317526e39ebb</elementGuidId>
    <selectorCollection>
       <entry>
          <key>BASIC</key>
+         <value></value>
       </entry>
       <entry>
          <key>XPATH</key>
-         <value>//a[contains(text(),'Nikhil Gosavi')]/parent::div/parent::div/parent::div/parent::div/div[3]/a[contains(text(),'Prescribe')]</value>
+         <value>//a[contains(text(),'Nikhil Gosavi')]/parent::div/parent::div/div[3]/a[contains(text(),'Disenroll Client')]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
